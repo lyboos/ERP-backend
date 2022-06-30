bfa0e1d57ea8c33b9fc2b5edfcb9b0d4c4646206
201250184 <201250184@smail.nju.edu.cn>
2022-06-30 23:58:31 +0800

    sale return

--- src/main/java/com/nju/edu/erp/service/Impl/SaleReturnsServiceImpl.java
+++ src/main/java/com/nju/edu/erp/service/Impl/SaleReturnsServiceImpl.java
@@ -1,25 +1,90 @@
 package com.nju.edu.erp.service.Impl;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.nju.edu.erp.dao.ProductDao;
+import com.nju.edu.erp.dao.SaleReturnsSheetDao;
+import com.nju.edu.erp.dao.SaleSheetDao;
+import com.nju.edu.erp.dao.WarehouseDao;
 import com.nju.edu.erp.enums.sheetState.PurchaseReturnsSheetState;
 import com.nju.edu.erp.enums.sheetState.SaleReturnsSheetState;
+import com.nju.edu.erp.enums.sheetState.SaleSheetState;
+import com.nju.edu.erp.model.po.SaleReturnsSheetContentPO;
+import com.nju.edu.erp.model.po.SaleReturnsSheetPO;
+import com.nju.edu.erp.model.po.SaleSheetContentPO;
+import com.nju.edu.erp.model.vo.ProductInfoVO;
+import com.nju.edu.erp.model.vo.SaleReturns.SaleReturnsSheetContentVO;
 import com.nju.edu.erp.model.vo.SaleReturns.SaleReturnsSheetVO;
 import com.nju.edu.erp.model.vo.UserVO;
+import com.nju.edu.erp.service.CustomerService;
+import com.nju.edu.erp.service.ProductService;
 import com.nju.edu.erp.service.SaleReturnsService;
+import com.nju.edu.erp.service.WarehouseService;
+import com.nju.edu.erp.utils.IdGenerator;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
 
+@Service
 public class SaleReturnsServiceImpl implements SaleReturnsService {
 
-    //SaleReturnsSheetDao saleReturnsSheetDao;
+    SaleReturnsSheetDao saleReturnsSheetDao;
+
+    ProductService productService;
+
+    ProductDao productDao;
+
+    SaleSheetDao saleSheetDao;
+
+    CustomerService customerService;
+
+    WarehouseService warehouseService;
+
+    WarehouseDao warehouseDao;
+
+    @Autowired
+    public SaleReturnsServiceImpl(SaleReturnsSheetDao saleReturnsSheetDao, ProductService productService, ProductDao productDao, SaleSheetDao saleSheetDao, CustomerService customerService, WarehouseService warehouseService, WarehouseDao warehouseDao) {
+        this.saleReturnsSheetDao = saleReturnsSheetDao;
+
+        this.productService = productService;
+
+        this.productDao = productDao;
+
+        this.saleSheetDao = saleSheetDao;
+
+        this.customerService = customerService;
+
+        this.warehouseService = warehouseService;
+
+        this.warehouseDao = warehouseDao;
+    }
     /**
      * 制定销售退货单
      *
      * @param saleReturnsSheetVO 销售退货单
      */
     @Override
+    @Transactional
     public void makeSaleReturnsSheet(UserVO userVO, SaleReturnsSheetVO saleReturnsSheetVO) {
+        SaleReturnsSheetPO saleReturnsSheetPO = new SaleReturnsSheetPO();
+        BeanUtils.copyProperties(saleReturnsSheetVO, saleReturnsSheetPO);
 
+        saleReturnsSheetPO.setOperator(userVO.getName());
+        saleReturnsSheetPO.setCreateTime(new Date());
+        SaleReturnsSheetPO latest = saleReturnsSheetDao.getLatest();
+        String id = IdGenerator.generateSheetId(latest == null ? null : latest.getId(), "XSTHD");
+        saleReturnsSheetPO.setSaleSheetId(id);
+        saleReturnsSheetPO.setState(SaleSheetState.PENDING_LEVEL_1);
+        BigDecimal rawTotalAmount = BigDecimal.ZERO;
+        List<SaleSheetContentPO> saleSheetContents = saleSheetDao.findContentBySheetId(saleReturnsSheetPO.getSaleSheetId());
+        Map<String, SaleSheetContentPO> map = new HashMap<>();
+        for (SaleSheetContentPO item : saleSheetContents) {
+            map.put(item.getPid(), item);
+        }
     }
 
     /**
@@ -29,8 +94,28 @@ public class SaleReturnsServiceImpl implements SaleReturnsService {
      * @return 销售退货单
      */
     @Override
-    public List<SaleReturnsSheetVO> getSaleReturnsSheetByState(PurchaseReturnsSheetState state) {
-        return null;
+    public List<SaleReturnsSheetVO> getSaleReturnsSheetByState(SaleReturnsSheetState state) {
+        List<SaleReturnsSheetVO> res = new ArrayList<>();
+        List<SaleReturnsSheetPO> all;
+        if (state == null) {
+            all = saleReturnsSheetDao.findAll();
+        } else {
+            all = saleReturnsSheetDao.findAllByState(state);
+        }
+        for (SaleReturnsSheetPO po: all) {
+            SaleReturnsSheetVO vo = new SaleReturnsSheetVO();
+            BeanUtils.copyProperties(po, vo);
+            List<SaleReturnsSheetContentPO> alll = saleReturnsSheetDao.findContentBySaleReturnsSheetId(po.getId());
+            List<SaleReturnsSheetContentVO> vos = new ArrayList<>();
+            for (SaleReturnsSheetContentPO p: alll) {
+                SaleReturnsSheetContentVO v = new SaleReturnsSheetContentVO();
+                BeanUtils.copyProperties(p, v);
+                vos.add(v);
+            }
+            vo.setSaleReturnsSheetContent(vos);
+            res.add(vo);
+        }
+        return res;
     }
 
     /**
@@ -41,7 +126,27 @@ public class SaleReturnsServiceImpl implements SaleReturnsService {
      * @param state              进货退货单修改后的状态
      */
     @Override
+    @Transactional
     public void approval(String saleReturnsSheetId, SaleReturnsSheetState state) {
+        SaleReturnsSheetPO saleReturnsSheet = saleReturnsSheetDao.findOneById(saleReturnsSheetId);
+        if (state.equals(SaleReturnsSheetState.FAILURE)) {
+            if (saleReturnsSheet.getState().equals(SaleSheetState.SUCCESS)) throw new RuntimeException("状态更新失败");
+            int effectLines = saleReturnsSheetDao.updateState(saleReturnsSheetId, state);
+            if (effectLines == 0) throw new RuntimeException("状态更新失败");
+        } else {
+            SaleReturnsSheetState prevState;
+            if (state.equals(SaleReturnsSheetState.SUCCESS)) {
+                prevState = SaleReturnsSheetState.PENDING_LEVEL_2;
+            } else if (state.equals(SaleReturnsSheetState.PENDING_LEVEL_2)) {
+                prevState = SaleReturnsSheetState.PENDING_LEVEL_1;
+            } else {
+                throw new RuntimeException("状态更新失败");
+            }
+            int effectLines = saleReturnsSheetDao.updateStateV2(saleReturnsSheetId, prevState, state);
+            if (effectLines == 0) throw new RuntimeException("状态更新失败");
+            if (state.equals(SaleReturnsSheetState.SUCCESS)) {
 
+            }
+        }
     }
 }
