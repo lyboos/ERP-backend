3e7944d823be53ecc7b4e7e497323b16e7db605d
201250119 <201250119@smail.nju.edu.cn>
2022-07-05 02:39:23 +0800

    fix: fix #352#,找到了mapper中存在的自增ID无法写入数据库，以及PO，VO对象混淆的问题

--- src/main/java/com/nju/edu/erp/web/controller/CustomerController.java
+++ src/main/java/com/nju/edu/erp/web/controller/CustomerController.java
@@ -1,7 +1,11 @@
 package com.nju.edu.erp.web.controller;
 
+import com.nju.edu.erp.auth.Authorized;
 import com.nju.edu.erp.enums.CustomerType;
+import com.nju.edu.erp.enums.Role;
 import com.nju.edu.erp.model.vo.CustomerVO;
+import com.nju.edu.erp.model.vo.UserVO;
+import com.nju.edu.erp.model.vo.purchase.PurchaseSheetVO;
 import com.nju.edu.erp.service.CustomerService;
 import com.nju.edu.erp.web.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,14 @@ public class CustomerController {
     @GetMapping("/findByType")
     public Response findByType(@RequestParam CustomerType type) {
         return Response.buildSuccess(customerService.getCustomersByType(type));
+    }
+
+    @Authorized(roles = {Role.SALE_STAFF, Role.SALE_MANAGER, Role.GM, Role.ADMIN})
+    @PostMapping("/sheet-make")
+    public Response insertCustomer(@RequestBody CustomerVO customerVO){
+        System.out.println("has triggered controller");
+        customerService.insertCustomer(customerVO);
+        return Response.buildSuccess();
     }
 
 }
