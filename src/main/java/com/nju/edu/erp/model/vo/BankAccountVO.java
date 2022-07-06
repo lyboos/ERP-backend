b367c08618fa77147e27a57feb6d3de267700881
201250119 <201250119@smail.nju.edu.cn>
2022-07-06 11:28:04 +0800

    add vo,po to bankaccount

---
+++ src/main/java/com/nju/edu/erp/model/vo/BankAccountVO.java
@@ -0,0 +1,29 @@
+package com.nju.edu.erp.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BankAccountVO {
+    /**
+     * 账户id
+     */
+    private String id;
+
+    /**
+     * 账户名
+     */
+    private String name;
+
+    /**
+     * 账户余额，不可直接修改！只能通过payment/receipt进行变动
+     */
+    private BigDecimal remaining_sum;
+}
