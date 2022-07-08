8c19f9c75ffd6361cfe6a23d17c260a841eaf2c0
201250184 <201250184@smail.nju.edu.cn>
2022-07-08 11:31:24 +0800

    add SaleDetail

--- src/main/java/com/nju/edu/erp/service/SaleDetailSheetService.java
+++ src/main/java/com/nju/edu/erp/service/SaleDetailSheetService.java
@@ -1,8 +1,10 @@
 package com.nju.edu.erp.service;
 
+import com.nju.edu.erp.model.vo.SaleDetailReplyVO;
+import com.nju.edu.erp.model.vo.SaleDetailRequestVO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface SaleDetailSheetService {
-
+    SaleDetailReplyVO getSaleDetails(SaleDetailRequestVO request);
 }
