d755355f07f1dd9e9b18a76b228fb31f86e38b1b
201250119 <201250119@smail.nju.edu.cn>
2022-07-10 01:48:53 +0800

    fix bugs in strategy

--- src/main/java/com/nju/edu/erp/web/controller/SaleStrategyController.java
+++ src/main/java/com/nju/edu/erp/web/controller/SaleStrategyController.java
@@ -26,8 +26,9 @@ public class SaleStrategyController {
      * 对不同等级客户实行促销
      */
     @Authorized(roles = {Role.GM, Role.ADMIN})
-    @PostMapping(value = "/user-level")
-    public Response user_level(@RequestParam(value = "level") String level,@RequestParam(value = "discount") String discount)  {
+    @GetMapping(value = "/user-level")
+    public Response user_level(@RequestParam(value = "level") String level,
+                               @RequestParam(value = "discount") String discount)  {
         SaleService.getUserStrategy(BigDecimalsArrayUtil.getBigDecimals(level,discount));
         return Response.buildSuccess();
     }
@@ -35,9 +36,10 @@ public class SaleStrategyController {
     /**
      * 对一定额度金额实行促销
      */
+    @GetMapping(value = "/additional")
     @Authorized(roles = {Role.GM, Role.ADMIN})
-    @PostMapping(value = "/additional")
-    public Response additional(@RequestParam(value = "additionalDiscount") String additionalDiscount,@RequestParam(value = "trigger") String trigger)  {
+    public Response additional(@RequestParam(value = "additionalDiscount") String additionalDiscount,
+                               @RequestParam(value = "trigger") String trigger)  {
         SaleService.getAdditionalDiscountStrategy(new BigDecimal(additionalDiscount),new BigDecimal(trigger));
         return Response.buildSuccess();
     }
@@ -45,8 +47,8 @@ public class SaleStrategyController {
     /**
      * 对一定额度金额赠送商品
      */
+    @GetMapping(value = "/give-away")
     @Authorized(roles = {Role.GM, Role.ADMIN})
-    @PostMapping(value = "/give-away")
     public Response give_away(@RequestParam(value = "trigger") String trigger,@RequestParam(value = "numOfGiveaway") String numOfGiveaway)  {
         SaleService.getGiveawayStrategy(new BigDecimal(trigger), Integer.valueOf(numOfGiveaway));
         return Response.buildSuccess();
