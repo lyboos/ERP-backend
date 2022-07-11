c2b40481a9542dfe573c2badb712edacdc1c0e8b
Koala-H <73047557+Koala-H@users.noreply.github.com>
2022-07-11 19:58:01 +0800

    add:a new PaymentTset

--- src/test/java/com/nju/edu/erp/service/PaymentServiceTest.java
+++ src/test/java/com/nju/edu/erp/service/PaymentServiceTest.java
@@ -2,6 +2,7 @@ package com.nju.edu.erp.service;
 
 import com.nju.edu.erp.enums.sheetState.RandPState;
 import com.nju.edu.erp.model.vo.Payment.PaymentVO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,7 @@ class PaymentServiceTest {
     @Test
     @Transactional
     @Rollback
-    void getSheetByState() {
+    void getSheetByState_1() {//这里需要配合数据库使用
         List<PaymentVO> res = paymentService.getSheetByState(RandPState.SUCCESS);
         for (PaymentVO paymentVO:res){
             System.out.println(paymentVO.getSupplier());
@@ -26,10 +27,19 @@ class PaymentServiceTest {
         }
     }
 
+    @Test
+    @Transactional
+    @Rollback
+    void getSheetById_1() {
+        PaymentVO paymentVO = paymentService.getSheetById("FKD-000001-002");
+        Assertions.assertTrue(paymentVO == null);
+    }
 
     @Test
-    void getSheetById() {
-        PaymentVO paymentVO = paymentService.getSheetById("FKD-20220703-001");
+    @Transactional
+    @Rollback
+    void getSheetById_2() {
+        PaymentVO paymentVO = paymentService.getSheetById("FKD-000001-001");
         System.out.println(paymentVO.getSupplier());
         System.out.println(paymentVO.getState());
     }
